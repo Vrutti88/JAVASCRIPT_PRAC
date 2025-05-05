ccee9aaa5a1896e6f1bb4a2f322385ca7e62afc6
Vrutti Patil <vruttipatil1396@gmail.com>
2025-05-05 22:05:39 +0530

    Add files via upload

---
+++ may5_2.js
@@ -0,0 +1,35 @@
+let x=10;
+let y=10e40;
+let z="Hello 8+2";
+let isTrue=true;
+let arr=[1,2,3,4];
+let obj={name:"Vrutti", age:"18"};
+let func=function(){return "Hello World"};
+let nullValue=null;
+let undefinedValue;
+let symbolValue=Symbol("w")
+let bigIntValue=BigInt(465267452764527572576254825482684365835826356438538);
+let dataValue=new Date();
+let demo=`the sum of numbers is: ${3+5}`;
+
+console.log("x",x);
+console.log("y",y);
+console.log("z",z);
+console.log("isTrue",isTrue);
+console.log("arr",arr);
+console.log("obj",obj);
+console.log("func",func);
+console.log("nullValue",nullValue);
+console.log("undefinedValue",undefinedValue);
+console.log("symbolValue",symbolValue);
+console.log("bigIntValue",bigIntValue);
+console.log("dataValue",dataValue);
+console.log("demo",demo);
+
+
+
+let x_=5, y_="5";
+console.log(x_===y_);
+console.log(x_==y_);
+console.log(x_!=y_);
+console.log(x_!==y_);
