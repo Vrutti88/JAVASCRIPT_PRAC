41b89cb2e0fa4f0ef3bdf76661f390bb3c7b85d1
Vrutti Patil <vruttipatil1396@gmail.com>
2025-05-05 22:06:44 +0530

    Add files via upload

---
+++ may5_5.js
@@ -0,0 +1,20 @@
+function f1(username){
+    console.log(username)
+}
+f1=(prompt("Enter your user name:"))
+
+function f2(password){
+    console.log(password);
+}
+f2=(prompt("Enter you password:"));
+
+if(f2=="1234"){
+    document.writeln("Correct");
+}
+else{
+    document.writeln("InCorrect");
+}
+
+
+
+
