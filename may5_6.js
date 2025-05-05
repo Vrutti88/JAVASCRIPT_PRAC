6353a68d33cbfb1b7be68101163eeb2850edfc43
Vrutti Patil <vruttipatil1396@gmail.com>
2025-05-05 22:07:37 +0530

    Add files via upload

---
+++ may5_6.js
@@ -0,0 +1,23 @@
+let value=7
+for(let i=0; i<11;i++){
+    console.log(`${value} * ${i} = ${value*i}`)
+}
+
+
+
+function f1(num){
+    console.log(num)
+}
+f1=prompt("Enter a number:")
+for(let i=0; i<11;i++){
+    console.log(`${f1} * ${i} = ${f1*i}`)
+}
+
+
+
+let value2=5
+let i=0
+while(i<11){
+    console.log(`${value2} * ${i} = ${value2*i}`)
+    i++;
+}
