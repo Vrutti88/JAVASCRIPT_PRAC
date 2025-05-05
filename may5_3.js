3ed116c63a07af143e0cea332412f305f5924562
Vrutti Patil <vruttipatil1396@gmail.com>
2025-05-05 22:06:05 +0530

    Add files via upload

---
+++ may5_3.js
@@ -0,0 +1,28 @@
+let a = 10;
+if(a%2==0){
+    console.log("Even")
+}
+
+
+let b = 13;
+if(b%2==0){
+    console.log("Even")
+}
+else{
+    console.log("Odd")
+}
+
+
+let c= 10
+if(c>10){
+    console.log("Greater")
+}
+else if(c<10){
+    console.log("Smaller")
+}
+else if(c==10){
+    console.log("Equal")
+}
+else{
+    console.log("None")
+}
