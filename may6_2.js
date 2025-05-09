0985a68b0417453526445245ed346051a3e06a13
Vrutti Patil <vruttipatil1396@gmail.com>
2025-05-09 12:55:42 +0530

    Add files via upload

---
+++ may6_2.js
@@ -0,0 +1,20 @@
+function inp1(num){
+    console.log()
+}
+let num1 = prompt("Enter first number");
+inp1(num1);
+
+function inp2(num){
+    console.log()
+}
+let num2 = prompt("Enter second number");
+inp2(num2);
+
+let res = Number(num1) + Number(num2);
+console.log("Result:", res);
+
+
+
+
+
+
