39b6823ccce29d59b345a629ab180768b3575566
Vrutti Patil <vruttipatil1396@gmail.com>
2025-05-09 13:00:24 +0530

    Add files via upload

---
+++ may6_8.js
@@ -0,0 +1,12 @@
+let arr=[1,2,3,4,5,6,7,8,9,10]
+console.log(arr.length)
+console.log(arr.push(11))
+console.log(arr.pop())
+console.log(arr.shift())
+console.log(arr.unshift(0))
+console.log(arr.slice(0,5))
+console.log(arr.splice(0,8))
+console.log(arr.flat())
+console.log(arr.sort())
+console.log(arr.reverse())
+console.log(arr.toSorted())
