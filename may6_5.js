eb67e3d3fc8d84c1bab20e66b4c75ed947eb1a6d
Vrutti Patil <vruttipatil1396@gmail.com>
2025-05-09 12:58:34 +0530

    Add files via upload

---
+++ may6_5.js
@@ -0,0 +1,16 @@
+function f1(num){
+    console.log(num)
+}
+console.log(f1="hello")
+
+let func=function (num1, num2){
+    console.log(num1)
+}
+console.log(func(3,4,5,6))
+
+
+
+const func2=()=>{
+    console.log("hello")
+}
+func2()
