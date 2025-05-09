73d1555480c76415ae23699745479ba2e64bb0bd
Vrutti Patil <vruttipatil1396@gmail.com>
2025-05-09 12:56:13 +0530

    Add files via upload

---
+++ may6_3.js
@@ -0,0 +1,17 @@
+function f1(inp1){
+    console.log(inp1)
+}
+f1=prompt("Enter a string")
+
+function f2(inp2){
+    console.log(inp2)
+}
+f2=prompt("Enter a string")
+
+
+if(f1.length==f2.length){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
