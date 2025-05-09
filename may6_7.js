41d117072bbd155db8194f59443e13be1fc616b1
Vrutti Patil <vruttipatil1396@gmail.com>
2025-05-09 12:59:45 +0530

    Add files via upload

---
+++ may6_7.js
@@ -0,0 +1,15 @@
+let arr=[1,2,3,4,5]
+console.log(arr[0])
+
+let arr1=new Array("a", "b", "c", "d")
+console.log(arr[0])
+
+
+for(let i=0; i<arr.length; i++){
+    console.log(arr[i])
+}
+
+
+for(let i=0; i<arr.length; i--){
+    console.log(arr[i])
+}
