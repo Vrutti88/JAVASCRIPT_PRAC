fc300c4642e76bec8145def22944f924e023837d
Vrutti Patil <vruttipatil1396@gmail.com>
2025-05-14 11:43:30 +0530

    Add files via upload

---
+++ may7_4.js
@@ -0,0 +1,26 @@
+document.getElementsByClassName("myclass")[0].innerHTML="Hello Sir"
+document.getElementById("myid").class="myclass"
+
+//mouse events
+onclick()
+mouseup()
+mousedown()
+hover()
+
+
+//keyboard events
+keyup()
+keydown()
+
+
+//form event
+onsubmit()
+onreset()
+focus()
+blur()
+change()
+
+
+//brower event
+onload()
+onrefresh()
